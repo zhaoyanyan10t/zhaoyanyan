a50512872b17b57d5a68864c8b63cf219239e4ea
赵妍妍 <1670787414@qq.com>
2019-12-18 16:26:32 +0800

    20191218

---
+++ Project40/Project40/0.C
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS     
+#include <stdio.h> 
+#include<assert.h>
+char * strcpy(char * dst, const char * src) {
+	char * p = dst;
+	while (*p != '\0')
+	{
+		p++;
+		src++;
+	}
+	return src;
+}
+
+int main()
+{
+	char p[] = "abcdef";
+	char *src = NULL;
+	int l = strcpy(p, src);
+	printf("%s\n", l);
+	system("pause");
+	return 0;
+}
+//int my_strlen(const char *str) 
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while(*str)//ÅÐ¶Ï×Ö·û´®ÊÇ·ñ½áÊø
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	const char* p = "abcdef"; //²âÊÔ
+//	int len = my_strlen(p);
+//	printf("len = %d\n", len);
+//	system("pause");
+//	return 0; 
+//}
