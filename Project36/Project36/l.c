284da8b518fc678ba190d1ee3b8cfcbfd1f95098
赵妍妍 <1670787414@qq.com>
2019-12-28 20:11:18 +0800

    20191228

---
+++ Project36/Project36/l.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+
+{
+	int i;
+	printf("输入一个数:");
+	scanf("%d", &i);
+	int m, n;
+	m = i % 10;
+	printf("%d\n", s);
+	system("pause");
+	return 0;
+}
