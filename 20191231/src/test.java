a11e46dcc4c206c3f4f78649c731d3c47a93948b
赵妍妍 <1670787414@qq.com>
2019-12-31 21:44:48 +0800

    20191231

---
+++ 20191231/src/test.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int GetUglyNumber_Solution(int index) {
+
+
+        return 0;
+    }
+}
+//class Solution {
+//    public int FindGreatestSumOfSubArray(int[] array) {
+//        if(array.length<1){
+//            return 0;
+//        }
+//        int max=array[0];
+//        for (int i = 0; i <array.length ; i++) {
+//            int sum=0;
+//            for (int j = i; j <array.length ; j++) {
+//                sum+=array[j];
+//                if(max<sum){
+//                    max=sum;
+//                }
+//            }
+//        }
+//        return max;
+//    }
+//}
+public class test {
+    public static void main(String[] args) {
+//        int[] arr={-2,-8,-1,-5,-9};
+//        Solution e=new Solution();
+//        System.out.println(e.FindGreatestSumOfSubArray(arr));
+    }
+}
