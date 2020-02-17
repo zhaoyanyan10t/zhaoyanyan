1c2fd024279ef83418ba550d94e43645748d444d
赵妍妍 <1670787414@qq.com>
2020-02-17 20:10:47 +0800

    20200217

---
+++ Project2/Project2/h.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<windows.h>
+int main()
+{
+	int i;
+	for (i = 1000; i <= 2000; i++)
+	{
+		if (((i % 4) == 0 && (i % 100) != 0) || (i % 400) == 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	system("pause");
+	return 0;
+}
