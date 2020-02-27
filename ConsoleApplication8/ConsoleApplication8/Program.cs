8217bc7843d7564505f1212d89490a0492035ee1
赵妍妍 <1670787414@qq.com>
2020-02-27 19:51:38 +0800

    20200227

---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1000;
+            double pi = 0d;
+            double tempPi = 1d;
+            double fz, fm1, fm2;
+            for (int i = 1; i <= n; i++)
+            {
+                fz = 2 * i;
+                fm1 = fz - 1;
+                fm2 = fz + 1;
+                tempPi *= fz / fm1 * fz / fm2;
+            }
+            pi = tempPi * 2;
+            Console.WriteLine(pi);
+            Console.Read();
+        }
+    }
+}
