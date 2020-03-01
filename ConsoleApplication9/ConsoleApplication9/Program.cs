2097c5d896c2e6870a9c3d94ab8e0f803f5d5fd5
赵妍妍 <1670787414@qq.com>
2020-03-01 15:15:38 +0800

    20200301

---
+++ ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 98765;
+            Console.Write(a.ToString("X"));
+            Console.Read();
+        }
+    }
+}
