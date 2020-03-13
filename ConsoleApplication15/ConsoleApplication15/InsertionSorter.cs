cf2e6f78b8cf2f179a1f649dced1fab6a6484ff0
赵妍妍 <1670787414@qq.com>
2020-03-13 21:41:05 +0800

    20200313

---
+++ ConsoleApplication15/ConsoleApplication15/InsertionSorter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication15
+{
+    class InsertionSorter
+    {
+        public int[] list { get; set; }
+
+        internal void Sort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
