e0ddb871867d8ea83cdfcbf42fd33559f370dd35
赵妍妍 <1670787414@qq.com>
2020-04-17 23:12:54 +0800

    20200417

---
+++ Experiment/Exptsite/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exptsite
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IbIMsg.Text = "我开始学习ASP.NET了！";
+        }
+    }
+}
