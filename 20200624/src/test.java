64aa71355bc7bf36ed43a873de85a1ba007f4635
赵妍妍 <1670787414@qq.com>
2020-07-12 20:55:46 +0800

    20200712

---
+++ 20200624/src/test.java
@@ -0,0 +1,31 @@
+public class test {
+}
+//1.
+//  INPUT "请输入长" TO X
+//  INPUT "请输入宽" TO Y
+//  L=2*(X+Y)
+//  S=X*Y
+//  ?"周长和面积："L,S
+//2
+//SET TALK OFF
+//CLEAR
+//S=0
+//for i=1 to 1000 step 2
+//S=S+i
+//endfor
+//? "偶数的和为：",S
+//        SET TALK ON
+//        RETURN
+//3.
+// CLEAR
+// SET TALK OFF
+//INPUT "请输入X" TO X
+//if x>0
+//Y=2X^2+3X+5
+//ELSE
+//Y=10X-5
+//ENDIF
+//? "X=",X
+//? "f(",X,")=",Y
+//SET TALK ON
+//RETURN
