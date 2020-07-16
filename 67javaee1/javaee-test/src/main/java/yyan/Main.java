72e9353e4a4042c3c2d0f7be69c185e62c8fadab
赵妍妍 <1670787414@qq.com>
2020-07-16 20:52:51 +0800

    20200716

---
+++ 67javaee1/javaee-test/src/main/java/yyan/Main.java
@@ -0,0 +1,10 @@
+package yyan;
+
+import org.springframework.context.ApplicationContext; import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applications.xml");
+    } }
+
